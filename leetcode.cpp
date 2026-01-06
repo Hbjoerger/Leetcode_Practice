65c7c2094a54a19ef993ce725526126085d496a0
Haley Joerger <Hbjoerger@yahoo.com>
2026-01-06 10:44:35 -0800

    Update leetcode.cpp
    Solved problem 387: First Unique Character in a string.

--- leetcode.cpp
+++ leetcode.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution {
 public:
+
     // Problem One: Add Digits
     int addDigits(int num) {
         while (num >= 10) {          
@@ -15,6 +16,20 @@ public:
         }
         return num;                  
     }
+
+    // Problem Two: First Unique Character in a String
+    int firstUniqChar(string s) {
+        int count[26] = {0};
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {                  
+            if (count[s[i] - 'a'] == 1) {                      
+                return i;                                     
+            }
+        }
+        return -1;                                 
+    }   
 };
 
 
